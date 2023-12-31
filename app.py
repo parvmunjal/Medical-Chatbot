6e4b0d4b2d453d84c1d01dde24cc3d6c07355691
parvmunjal <parvmunjal100@gmail.com>
2023-12-31 15:56:14 +0530

    Final project updated

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template,jsonify,request
+from src.helper import download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
+from src.prompt import * 
+import os
+
+app=Flask(__name__)
+load_dotenv()
+
+PINECONE_API_KEY= os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV= os.environ.get('PINECONE_API_ENV')
+
+embeddings= download_hugging_face_embeddings()
+
+
+#Initialising Pinecone
+pinecone.init(api_key=PINECONE_API_KEY,
+              environment=PINECONE_API_ENV)
+
+index_name="medical-chatbot"
+
+docsearch=Pinecone.from_existing_index(index_name,embeddings)
+
+PROMPT=PromptTemplate(template=prompt_template,input_variables=["context","question"])
+chain_type_kwargs={"prompt":PROMPT}
+
+llm=CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+                  model_type="llama",
+                  config={'max_new_tokens':512,
+                          'temperature':0.8})
+qa=RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=docsearch.as_retriever(search_kwargs={'k':2}),
+    return_source_documents=True,
+    chain_type_kwargs=chain_type_kwargs
+)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+@app.route("/get",methods=["GET","POST"])
+def chat():
+    msg = request.form["msg"]
+    input_msg = msg.lower()  # Convert the input to lowercase for case-insensitive matching
+
+    # Check if the user's message contains a question about the founder
+    if "who is your founder" in input_msg:
+        response = "This chatbot is created by Parv Munjal."
+    else:
+        # If the input doesn't match any hardcoded commands, proceed with the existing question-answering logic
+        result = qa({"query": msg})
+        response = str(result["result"])
+
+    return jsonify(response=response)
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
